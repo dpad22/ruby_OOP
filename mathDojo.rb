c1f25ac62724860c3ab359fa6f4bdf1d78f5a97e
David Padilla <dpadman2@gmail.com>
2020-06-03 15:18:16 -0500

    mathDojo

---
+++ mathDojo.rb
@@ -0,0 +1,35 @@
+class MathDojo
+
+    attr_accessor :sum
+    def initialize
+        @sum = 0
+    end
+
+    def add (*number)
+        if number.class == Array 
+			@sum += number.flatten.reduce(:+)
+		else
+            @sum += number
+        end
+        self
+    end
+
+    def subtract (*number)
+        if number.class == Array 
+			@sum -= number.flatten.reduce(:+)
+		else
+            @sum -= number
+        end
+        self
+    end
+
+    def result
+        puts @sum
+        self
+    end
+end
+
+
+
+challenge1 = MathDojo.new.add(2).add(2, 5).subtract(3, 2).result # => 4
+challenge2 = MathDojo.new.add(1).add([3, 5, 7, 8], [2, 4.3, 1.25]).subtract([2,3], [1.1, 2.3]).result # => 23.15
